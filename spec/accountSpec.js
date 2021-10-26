0948cbb140c985170c42b31de4a8fd8091259ee9
Tom Lee <tomalee2002@gmail.com>
2021-10-26 16:18:21 +0100

    adds spec test for account deposit

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -5,4 +5,8 @@ describe('Bank account', function () {
   it('is created with an initial balance of 0', function () {
     expect(account.balance).toBe(0);
   });
+  it('can be deposited into', function () {
+    account.deposit(10);
+    expect(account.balance).toBe(10);
+  });
 });
