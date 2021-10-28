1850818b6531dcca8bc19969f9fe06452a601d80
Tom Lee <tomalee2002@gmail.com>
2021-10-28 22:28:57 +0100

    aspec for statement handling deposits

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -2,9 +2,16 @@ describe('Account statement', function () {
   beforeEach(function () {
     statement = new Statement();
     console.log = jasmine.createSpy('log');
+    const mockDeposit = [['28/10/21', 'deposit', 10, 40]]; // Mock input date, transaction type, amount in/out and balance
   });
   it('can outputs the correct headers', function () {
     statement._outputHeader();
     expect(console.log).toHaveBeenCalledWith('DATE | AMOUNT | BALANCE\n');
+  });
+  it('can output a deposit properly', function () {
+    statement.getStatement(mockDeposit);
+    expect(console.log).toHaveBeenCalledWith(
+      'DATE | AMOUNT | BALANCE\n28/10/21 | +£10 | £40'
+    );
   });
 });
