c40f1c4d3a0d1c18bd7c7a0ddec9e864cdbddef5
Tom Lee <tomalee2002@gmail.com>
2021-10-29 12:59:29 +0100

    statement can handle deposit

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -2,7 +2,6 @@ describe('Account statement', function () {
   beforeEach(function () {
     statement = new Statement();
     console.log = jasmine.createSpy('log');
-    const mockDeposit = [['28/10/21', 'deposit', 10, 40]]; // Mock input date, transaction type, amount in/out and balance
   });
   it('can outputs the correct headers', function () {
     statement._outputHeader();
@@ -11,9 +10,11 @@ describe('Account statement', function () {
     );
   });
   it('can output a deposit properly', function () {
+    const mockDeposit = [['28/10/21', 'deposit', 10, 40]]; // Mock input date, transaction type, amount in/out and balance
     statement.getStatement(mockDeposit);
     expect(console.log).toHaveBeenCalledWith(
-      'DATE | TYPE | AMOUNT | BALANCE\n28/10/21 | +£10 | £40'
+      'DATE | TYPE | AMOUNT | BALANCE\n'
     );
+    expect(console.log).toHaveBeenCalledWith('28/10/21 | deposit | 10 | 40');
   });
 });
--- src/statement.js
+++ src/statement.js
@@ -1,5 +1,14 @@
 class Statement {
   _outputHeader() {
-    console.log('DATE | AMOUNT | BALANCE\n');
+    console.log('DATE | TYPE | AMOUNT | BALANCE\n');
+  }
+
+  getStatement(transactions) {
+    this._outputHeader();
+    transactions.forEach((transaction) => {
+      console.log(
+        `${transaction[0]} | ${transaction[1]} | ${transaction[2]} | ${transaction[3]}`
+      );
+    });
   }
 }
